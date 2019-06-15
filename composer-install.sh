9bd5e31d760587fbdd60515e5d0f8c4e01579d2f
James Wade <jpswade@gmail.com>
2019-06-15 12:12:39 +0100

    fix: absolute path

--- composer-install.sh
+++ composer-install.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm --volume 'php-app/public:/app' composer install --ignore-platform-reqs --no-scripts
+docker run --rm --volume ${PWD}/php-app/public:/app composer install --ignore-platform-reqs --no-scripts
