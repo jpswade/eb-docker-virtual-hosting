e5aae4fc9bbdf8c66433b83a2152aeded7666459
James Wade <jpswade@gmail.com>
2019-06-15 14:56:32 +0100

    fix: use github

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 DEPLOY_PATH=suzuki4u
-VCS_REPO=git@bitbucket.org:jpswade/suzuki4u.git
+VCS_REPO=git@github.com:jpswade/suzuki4u.git
 RELEASE_TIMESTAMP=release-$(date +"%s")
 set -e
 cd php-app
 git clone ${VCS_REPO} ${DEPLOY_PATH}-${RELEASE_TIMESTAMP}
 [[ -d public ]] && rm -fr public
 ln -s ${DEPLOY_PATH}-${RELEASE_TIMESTAMP} public
+aws s3 cp s3://elasticbeanstalk-eu-west-1-226269639461/suzuki4u/.env public/.env
